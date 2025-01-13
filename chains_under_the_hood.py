6025dc525090d466ce22c196259c3b839f53137e
Eugene <36330890+Hardbow@users.noreply.github.com>
2025-01-13 14:03:08 +0300

    updated chain_under_the_hood.py

--- chains_under_the_hood.py
+++ chains_under_the_hood.py
@@ -11,10 +11,11 @@ model = ChatOpenAI(model=getenv("SMART_LLM"), api_key=getenv("OPENAI_API_KEY"))
 
 messages = [
     ("system", "You are a film director. You recommend good topic for a new movie in {film_genre} film genre"),
-    ("human", "Tell me short information for script about {theme}")
+    ("human", "Tell me four sentences for script about {theme}")
 ]
 
 prompt_template = ChatPromptTemplate.from_messages(messages)
+
 
 format_prompt = RunnableLambda(lambda x: prompt_template.format_prompt(**x))
 invoke_model = RunnableLambda(lambda x: model.invoke(x.to_messages()))
